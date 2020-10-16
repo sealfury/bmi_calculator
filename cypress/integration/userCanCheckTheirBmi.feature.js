f54bf1fb7973d107ef7260c645801e8e46c040bc
William <willspecker@gmail.com>
2020-10-16 15:56:04 +0200

    Adding an it block in cypress to test the functionality of the BMI calculator

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -7,4 +7,7 @@ describe('User can check their BMI', () => {
         cy.get('#height').type('186')
         cy.get('#Calculate').click()
     })
+    it('displays a BMI value of 26.01', () => {
+        cy.get('#results').should('contain', '26.01')
+    })
 })
