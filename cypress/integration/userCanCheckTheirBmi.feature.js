1f9b17a904dc13d8f931ad59df061e7d50ddd5e2
William <willspecker@gmail.com>
2020-10-16 14:55:38 +0200

    Adding first cypress tests, one of which should be passing but is not- to be updated shortly

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -1,5 +1,8 @@
+
 describe('User can check their BMI', () => {
     it('user can enter weight and height', () => {
         cy.visit('http://localhost:3001')
+        cy.contains('BMI Calculator')
+        cy.get('#weight').type('90')
     })
 })
