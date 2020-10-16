3e04caa8e4f44f4f954bb9e919b85a493c821887
William <willspecker@gmail.com>
2020-10-16 15:51:31 +0200

    Adjusting button in index.html to pass in test framework and adding a line in cypress to test the button

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -4,5 +4,7 @@ describe('User can check their BMI', () => {
         cy.visit('http://localhost:3001')
         cy.contains('BMI Calculator')
         cy.get('#weight').type('90')
+        cy.get('#height').type('186')
+        cy.get('#Calculate').click()
     })
 })
