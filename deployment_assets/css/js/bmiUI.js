315b0ff07b72cddf670f413aee1be9ade5a82a45
William <willspecker@gmail.com>
2020-10-16 17:20:27 +0200

    Testing some further CSS style adjustments and preparing html and js files to include a separate calculator for imperial units

--- deployment_assets/css/js/bmiUI.js
+++ deployment_assets/css/js/bmiUI.js
@@ -1,8 +1,8 @@
-const calculateBMI = () => {
-    let weightInput = document.getElementById("weight")
-    let heightInput = document.getElementById("height")
-    let results = calculateMetricBMI(parseInt(weightInput.value), parseInt(heightInput.value))
-    let message = getBMIClassification(parseFloat(results))
-    let displayResultsElement = document.getElementById('results')
-    displayResultsElement.innerHTML = `<h2>Your BMI Value Is: ${results} and you are deemed ${message}</h2>`
+const calculateBMImetric = () => {
+    let weightInput = document.getElementById("weightkg")
+    let heightInput = document.getElementById("heightcm")
+    let resultsMetric = calculateMetricBMI(parseInt(weightInput.value), parseInt(heightInput.value))
+    let message = getBMIClassification(parseFloat(resultsMetric))
+    let displayResultsElement = document.getElementById('resultsMetric')
+    displayResultsElement.innerHTML = `<h2>Your BMI Value Is: ${resultsMetric} and you are deemed ${message}</h2>`
 }
